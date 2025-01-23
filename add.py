b2da5c14bf5037463503d2eacaa0d9d1687f16ba
KLU-2200030549 <158989996+KLU-2200030549@users.noreply.github.com>
2025-01-23 12:45:48 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,8 @@
+#This program is addition of two numbers
+
+num1 =1.25[D[D[D[D 1.25
+num2 = 6.23
+
+sum = num1 + num2[D[D[D[[[[[C[C[C[C[C[C[C[C[+num2
+
+print('The sum {}[D0} and {1} is {2}'.format(num1,num2,sum))
